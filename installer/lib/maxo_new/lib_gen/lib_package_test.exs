6ddd2298c91c211d84f114897d6d0a17e8541794
Roman Heinrich <roman.heinrich@gmail.com>
2023-04-13 11:36:30 +0200

    Feat: test for lib_package generator

--- installer/lib/maxo_new/lib_gen/lib_package_test.exs
+++ installer/lib/maxo_new/lib_gen/lib_package_test.exs
@@ -127,4 +127,104 @@ defmodule MaxoNew.LibGen.LibPackageTest do
       """ <- Virtfs.read!(p.fs, "/maxo_gen/CHANGELOG.md")
     )
   end
+
+  test "with options" do
+    p = LibDesc.new("maxo_dbinspect", module: DbInspect, app: :db_inspect)
+    p |> LibPackage.prepare_project() |> LibPackage.generate()
+
+    auto_assert(
+      [
+        "/maxo_dbinspect",
+        "/maxo_dbinspect/.formatter.exs",
+        "/maxo_dbinspect/.github",
+        "/maxo_dbinspect/.github/workflows",
+        "/maxo_dbinspect/.github/workflows/ci.yml",
+        "/maxo_dbinspect/.gitignore",
+        "/maxo_dbinspect/CHANGELOG.md",
+        "/maxo_dbinspect/Makefile",
+        "/maxo_dbinspect/README.md",
+        "/maxo_dbinspect/bin",
+        "/maxo_dbinspect/bin/setup",
+        "/maxo_dbinspect/bin/test",
+        "/maxo_dbinspect/bin/test_iex",
+        "/maxo_dbinspect/config",
+        "/maxo_dbinspect/config/config.exs",
+        "/maxo_dbinspect/examples",
+        "/maxo_dbinspect/examples/example.exs",
+        "/maxo_dbinspect/examples/example.tape",
+        "/maxo_dbinspect/examples/gen_tape.exs",
+        "/maxo_dbinspect/lib",
+        "/maxo_dbinspect/lib/db_inspect",
+        "/maxo_dbinspect/lib/db_inspect.ex",
+        "/maxo_dbinspect/lib/db_inspect/application.ex",
+        "/maxo_dbinspect/lib/db_inspect_test.exs",
+        "/maxo_dbinspect/lib/test_helper.exs",
+        "/maxo_dbinspect/mix.exs",
+        "/maxo_dbinspect/test",
+        "/maxo_dbinspect/test/support",
+        "/maxo_dbinspect/test/support/mneme_defaults.ex",
+        "/maxo_dbinspect/test/test_helper.exs"
+      ] <- Virtfs.tree!(p.fs, "/")
+    )
+
+    auto_assert(
+      """
+      defmodule DbInspect.MixProject do
+        use Mix.Project
+        @github_url "https://github.com/maxohq/db_inspect"
+        @version "0.1.0"
+        @description "DbInspect description"
+
+        def project do
+          [
+            app: :db_inspect,
+            source_url: @github_url,
+            version: @version,
+            description: @description,
+            elixir: "~> 1.14",
+            elixirc_paths: elixirc_paths(Mix.env()),
+            test_paths: ["test", "lib"],
+            test_pattern: "*_test.exs",
+            start_permanent: Mix.env() == :prod,
+            deps: deps(),
+            package: package()
+          ]
+        end
+
+        # Run "mix help compile.app" to learn about applications.
+        def application do
+          [
+            extra_applications: [:logger],
+            mod: {DbInspect.Application, []}
+          ]
+        end
+
+        def elixirc_paths(:test), do: ["lib", "test/support"]
+        def elixirc_paths(_), do: ["lib"]
+
+        defp package do
+          [
+            files: ~w(lib mix.exs README* CHANGELOG*),
+            licenses: ["MIT"],
+            links: %{
+              "Github" => @github_url,
+              "Changelog" => "@github_url/blob/main/CHANGELOG.md"
+            }
+          ]
+        end
+
+        # Run "mix help deps" to learn about dependencies.
+        defp deps do
+          [
+            {:ex_doc, "~> 0.29", only: :dev, runtime: false},
+            {:maxo_test_iex, "~> 0.1", only: [:test]},
+            {:mneme, "~> 0.3", only: [:test]}
+          ]
+        end
+      end
+      """ <-
+        Virtfs.read!(p.fs, "/maxo_dbinspect/mix.exs")
+        |> String.replace(~S|#{@github_url}|, "@github_url")
+    )
+  end
 end
